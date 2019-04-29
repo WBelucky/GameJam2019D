3019411a0f66b6df5489d0402db99f178024687f
aktmt <38717635+aktmt@users.noreply.github.com>
2019-04-29 10:53:15 +0900

    [add] プレイヤーが動くようになりました

---
+++ GameJam2019D/Assets/Script/BulletCreater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum  BulletType
+{
+    Straight
+}
+public class BulletCreater 
+{
+    //オブジェクトプールを使うならここで拾ってくる処理を入れませう
+    private GameObject creatingBullet = new GameObject();
+
+    public GameObject CretateBullet(BulletType bulletType)
+    {
+        switch (bulletType)
+        {
+            case BulletType.Straight:
+                creatingBullet.AddComponent<StraightBullet>();
+                return creatingBullet;
+                
+            default:
+                Debug.LogError("そんな弾の種類は存在しませんenumに追加してクラスを作ってちょ");
+                return creatingBullet;
+        }
+
+    }
+    
+}
--- GameJam2019D/Assets/Script/Player.cs
+++ GameJam2019D/Assets/Script/Player.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static Player Instance;
+    private float HP = 1000.0f;
+    private float speed = 0.5f;
+    
+    // 移動可能な範囲
+    public static Vector2 m_moveLimit = new Vector2(50.0f, 50.0f);
+
+    //unity内で使う関数
+    //=============================
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+       
+    }
+    
     void Update()
     {
-        
+        PlayerMove();
+    }
+    //=============================
+
+    //ここからプレイヤーの持つメソッド部分
+
+    public void PlayerMove()
+    {
+        // 矢印キーの入力情報を取得する
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 矢印キーが押されている方向にプレイヤーを移動する
+        Vector3 velocity = new Vector3(h, v) * speed;
+        transform.localPosition += velocity;
+        transform.localPosition = ClampPosition(transform.localPosition);
+        // プレイヤーのスクリーン座標を計算する
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // プレイヤーから見たマウスカーソルの方向を計算する
+        Vector3 direction = Input.mousePosition - screenPos;
+
+        // マウスカーソルが存在する方向の角度を取得する
+        float angle = GetAngle(Vector3.zero, direction);
+
+        // プレイヤーがマウスカーソルの方向を見るようにする
+        Vector3 angles = transform.localEulerAngles;
+        angles.z = angle - 90;
+        transform.localEulerAngles = angles;
+    }
+
+
+    // 指定された位置を移動可能な範囲に収めた値を返す
+    public static Vector3 ClampPosition(Vector3 position)
+    {
+        return new Vector3
+        (
+            Mathf.Clamp(position.x, -m_moveLimit.x, m_moveLimit.x),
+            Mathf.Clamp(position.y, -m_moveLimit.y, m_moveLimit.y),
+            0
+        );
+    }
+    // 指定された 2 つの位置から角度を求めて返す
+    public static float GetAngle(Vector2 from, Vector2 to)
+    {
+        var dx = to.x - from.x;
+        var dy = to.y - from.y;
+        var rad = Mathf.Atan2(dy, dx);
+        return rad * Mathf.Rad2Deg;
     }
 }
