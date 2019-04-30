ae426c7e07e0c975c01148ab9e975b2d90a010c7
kadoken <kadoken0426@docomo.ne.jp>
2019-04-30 13:25:52 +0900

    add fadein and fadeout

---
+++ GameJam2019D/Assets/Script/FadeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    public float alfa=255;
+    float fadespeed = 0.01f;
+    float red, green, blue;
+
+    bool isFadingOut = false;
+    bool isFadingIn = false;
+
+    public void StartFadingOut()
+    {
+        isFadingOut = true;
+    }
+    public void StartfadingIn()
+    {
+        isFadingIn = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+        alfa = GetComponent<Image>().color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFadingOut)
+        {
+            alfa -= fadespeed;
+            GetComponent<Image>().color = new Color(red, green, blue, alfa);
+            
+        }
+        if (isFadingIn)
+        {
+            alfa += fadespeed;
+            GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        }
+        
+    }
+}
---
+++ GameJam2019D/Assets/Script/PictureManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PictureManager : MonoBehaviour
+{
+    public Image GameClearImage;
+    public Image BakusiImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float time1 = 5.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        time1 -= Time.deltaTime;
+       // if(time1 >= 0)
+        {
+            //BakusiImage.enabled = false;
+        }
+        if (time1 < 0)
+        {
+            //BakusiImage.enabled = false;
+            GameClearImage.GetComponent<FadeScript>().StartFadingOut();
+        }
+       if(time1 < -1)
+        {
+            BakusiImage.GetComponent<FadeScript>().StartfadingIn();
+            //BakusiImage.enabled = true;
+           //GameClearImage.enabled = false;
+        }
+    }
+}
