9a9123bc78fde1d6fe64d192e129aacc84382467
WBeLucky <shintaro.kusabiraki@gmail.com>
2019-05-03 17:37:23 +0900

    リザルトの画像がスコアに合わせて変更されるようになった

--- GameJam2019D/Assets/Script/GameClearSceneManager.cs
+++ GameJam2019D/Assets/Script/GameClearSceneManager.cs
@@ -6,6 +6,7 @@ public class GameClearSceneManager : MonoBehaviour
 {
     public Image GameClearImage;
     public Image BakusiImage;
+    public Image RyunenKaihiImage;
     public Text text;
     public Text score;  
     // Start is called before the first frame update
@@ -27,9 +28,15 @@ public class GameClearSceneManager : MonoBehaviour
             //BakusiImage.enabled = false;
             GameClearImage.GetComponent<FadeScript>().StartFadingOut();
         }
-       if(time1 < -1)
+        if(time1 < -1)
         {
-            BakusiImage.GetComponent<FadeScript>().StartfadingIn();
+            if(score.gameObject.GetComponent<DisplayScoreInText>().totalScore >= 20000)
+            {
+                RyunenKaihiImage.GetComponent<FadeScript>().StartfadingIn();
+            } else
+            {
+                BakusiImage.GetComponent<FadeScript>().StartfadingIn();
+            }
             //BakusiImage.enabled = true;
            //GameClearImage.enabled = false;
         }
